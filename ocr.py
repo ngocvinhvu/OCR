6299a992d82c3993b9f21fc1dae89353bfa3bcd1
vinhvn <vungocvinh95@gmail.com>
2022-12-01 15:45:49 +0700

    Add

--- ocr.py
+++ ocr.py
@@ -58,15 +58,41 @@ def extract_features(X):
     return [flatten_list(sample) for sample in X]
 
 
+def get_training_distance_for_test_sample(X_train, test_sample):
+    return [dist(train_sample, test_sample) for train_sample in X_train]
+
+
+def dist(x, y):
+    return sum(
+        [(bytes_to_int(x_i) - bytes_to_int(y_i)) ** 2 for x_i, y_i in zip(x, y)]
+    ) ** 0.5
+
+
+def knn(X_train, y_train, y_test, X_test, k=3): # need to return y_test
+    y_pred = []
+    for test_sample_idx, test_sample in enumerate(X_test):
+        training_distances = get_training_distance_for_test_sample(X_train, test_sample)
+        sorted_distance_indices = [
+            pair[0] for pair in sorted(enumerate(training_distances), key=lambda x: x[1])
+        ]
+        candidates = [
+            y_train[idx] for idx in sorted_distance_indices[:k]
+        ]
+        print(f"point is {y_test[test_sample_idx]} and we guess {candidates}")
+        y_sample = 5
+        y_pred.append(y_sample)
+    return y_pred
+
+
 def main():
-    X_train = read_images(TRAIN_DATA_FILENAME, 100)
+    X_train = read_images(TRAIN_DATA_FILENAME, 1000)
     y_train = read_labels(TRAIN_LABELS_FILENAME)
-    X_test = read_images(TEST_DATA_FILENAME, 100)
+    X_test = read_images(TEST_DATA_FILENAME, 5)
     y_test = read_labels(TEST_LABELS_FILENAME)
-    
+
     X_train = extract_features(X_train)
     X_test = extract_features(X_test)
-
+    knn(X_train, y_train, X_test, y_test, 3)
     pass
 
 
